b2b1895ccabcbededdf2420bf3f4a07c2401127f
PSI645 <paulovitor645@outlook.com>
2026-06-21 11:22:53 -0300

    alteração senha

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
   host:     process.env.DB_HOST     || "localhost",
   port:     parseInt(process.env.DB_PORT) || 3306,
   user:     process.env.DB_USER     || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "12345678",
   database: process.env.DB_NAME     || "HarpyToys",
   waitForConnections: true,
   connectionLimit: 10,
